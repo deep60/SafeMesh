864ae4c7bff63649dba08f5cd601120b08d97553
deep60 <deep6060p@gmail.com>
2026-02-23 16:34:30 +0530

    Initial Commit

---
+++ SafeMesh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SafeMesh
+//
+//  Created by P Deepanshu on 23/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SafeMesh/SafeMeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeMeshApp.swift
+//  SafeMesh
+//
+//  Created by P Deepanshu on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SafeMeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
